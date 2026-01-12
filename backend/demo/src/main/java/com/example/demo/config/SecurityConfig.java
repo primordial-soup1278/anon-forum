b29041a020166444d5c8674dae106412c4317bd5
jayden <jaydenthung@hotmail.com>
2026-01-12 09:11:30 -0800

    fix cors issue

--- backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -94,7 +94,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "https://anon-forum-two.vercel.app/"   ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
